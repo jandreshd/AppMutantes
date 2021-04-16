97a1c7eb49152cb6dd9c9e0ed38eb535f5596f70
jandreshd <jandreshd@gmail.com>
2021-04-15 23:55:59 -0500

    init

---
+++ application.py
@@ -0,0 +1,12 @@
+from flask import Flask
+application = Flask(__name__)
+# add a rule for the index page.
+@application.route('/')
+def hello_world():
+    return "Hola Mundo"
+    
+if __name__ == "__main__":
+ # Setting debug to True enables debug output. This line should be
+ # removed before deploying a production app.
+ application.debug = True
+ application.run()
